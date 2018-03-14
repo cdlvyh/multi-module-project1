6f769c6b4d154eda82bfeed63bd2bdf0f597d148
cdlvyh <lvyunheng0209@126.com>
2018-03-14 23:25:34 +0800

    test commit

--- web-service/src/main/java/com/cnblogs/SimpleService.java
+++ web-service/src/main/java/com/cnblogs/SimpleService.java
@@ -3,6 +3,7 @@ package com.cnblogs;
 public class SimpleService {
 
     public static String getServiceDescription() {
+        System.out.println("test");
         return "this is a simple service";
     }
 }
